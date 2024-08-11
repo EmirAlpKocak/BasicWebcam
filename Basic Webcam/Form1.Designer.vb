84406ce0fe55da0e2b3615d2b034ceff67c26093
EmirAlpKocak <168924880+EmirAlpKocak@users.noreply.github.com>
2024-08-11 21:26:10 +0300

    Add files via upload

---
+++ Basic Webcam/Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.EnableAlwaysOnTopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnableSmallerViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TakePhotoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(416, 290)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EnableAlwaysOnTopToolStripMenuItem, Me.EnableSmallerViewToolStripMenuItem, Me.TakePhotoToolStripMenuItem, Me.AboutToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(189, 114)
+        '
+        'EnableAlwaysOnTopToolStripMenuItem
+        '
+        Me.EnableAlwaysOnTopToolStripMenuItem.Checked = True
+        Me.EnableAlwaysOnTopToolStripMenuItem.CheckOnClick = True
+        Me.EnableAlwaysOnTopToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.EnableAlwaysOnTopToolStripMenuItem.Name = "EnableAlwaysOnTopToolStripMenuItem"
+        Me.EnableAlwaysOnTopToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.EnableAlwaysOnTopToolStripMenuItem.Text = "Enable Always on Top"
+        '
+        'EnableSmallerViewToolStripMenuItem
+        '
+        Me.EnableSmallerViewToolStripMenuItem.CheckOnClick = True
+        Me.EnableSmallerViewToolStripMenuItem.Name = "EnableSmallerViewToolStripMenuItem"
+        Me.EnableSmallerViewToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.EnableSmallerViewToolStripMenuItem.Text = "Enable Smaller View"
+        '
+        'TakePhotoToolStripMenuItem
+        '
+        Me.TakePhotoToolStripMenuItem.Name = "TakePhotoToolStripMenuItem"
+        Me.TakePhotoToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.TakePhotoToolStripMenuItem.Text = "Take Photo"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(188, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Timer1
+        '
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Filter = "PNG Files|*.png|All Files|*.*"
+        Me.SaveFileDialog1.Title = "Save Photo"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 290)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Basic Webcam"
+        Me.TopMost = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TakePhotoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents EnableAlwaysOnTopToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EnableSmallerViewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
